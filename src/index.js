69e8ba589ce8dfa0d1a186e19c24d80d69045728
Miguel Duarte <miguelfeliped@gmail.com>
2020-03-12 15:04:26 -0300

    3577: Removed AMBLoader (#3579)
    Co-authored-by: Ryan X. Charles <ryanxcharles@gmail.com>

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@
 import PropTypes from 'prop-types'
 import React, { Component } from 'react'
 import { MoneyButtonJs } from './money-button-js'
-export * from './IMBLoader'
 export { MoneyButtonJs }
 
 export default class MoneyButton extends Component {
