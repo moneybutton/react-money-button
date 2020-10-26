f5f43d20dd4df15d099f8823f4c9596ee5f69787
Miguel Duarte <miguelfeliped@gmail.com>
2020-10-26 17:02:51 -0300

    Move money button to root (#4202)
    * removed all other projects.
    
    * moved moneybutton to root.
    
    * fixed regtest
    
    * removed money-button folder from deploy scripts
    
    * vagrant files
    
    * improved vagrantfile.
    
    * removed yours references in travis.
    
    * more travis fixes
    
    * more fixes travis
    
    * removed old file

--- src/test.js
+++
@@ -1,7 +0,0 @@
-import MoneyButton from '.'
-
-describe('MoneyButton', () => {
-  it('is truthy', () => {
-    expect(MoneyButton).toBeTruthy()
-  })
-})
--- src/util/config.js
+++
@@ -1,7 +0,0 @@
-import MoneyButtonConfigBuilder from '@moneybutton/config'
-
-const config = new MoneyButtonConfigBuilder()
-  .addValueWithDefault('MONEY_BUTTON_IFRAME_LOADER_URI', process.env.MONEY_BUTTON_IFRAME_LOADER_URI, 'https://www.moneybutton.com/moneybutton.js')
-  .build()
-
-export default config
